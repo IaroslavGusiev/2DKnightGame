5e299fc7781c494a6d7fa0c5868c3c9863902246
IaroslavGusiev <86547184+IaroslavGusiev@users.noreply.github.com>
2021-06-30 20:14:24 +0300

    Event Sender and Event Recieve Test

---
+++ 2DKnightGame_Course/Assets/Scripts/EventSender.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class EventSender : MonoBehaviour
+{
+    public event Action<float> OnFloatSend;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnFloatSend.Invoke(5.5f);
+        }
+    }
+}
---
+++ 2DKnightGame_Course/Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+
+public class ScoreSystem : MonoBehaviour
+{
+    [SerializeField] private int _score;
+    [SerializeField] private int _dragonHitScore = 10;
+    [SerializeField] private int _dragonKillScore = 50;
+    [SerializeField] private GameController _gameController;
+
+    public int Score
+    {
+        get
+        {
+            return _score;
+        }
+        set
+        {
+            if (value != _score)
+            {
+                _score = value;
+                HUD.Instance.SetScore(_score.ToString());
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        _gameController.OnDragonWasHit += UpdatingScore;
+        _gameController.OnDragonWasKilled += UpdatingScore;
+    }
+
+    private void UpdatingScore(int score) 
+    {
+        _score += score;  
+    }
+
+
+}
